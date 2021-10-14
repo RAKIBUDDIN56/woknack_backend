10b7c0a9dbb29a28f2d621c0b8fb5c6dc512046d
Rakib Uddin <rsrakib404@gmail.com>
2021-10-14 16:00:41 +0530

    app updated

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ def videoToText():
 #url for mobile 
 #  host='192.168.8.100',port=5000
 if __name__ == "__main__":
-    app.run(debug=False, host='192.168.8.101',port=5000)
+    app.run(threaded=True,port=5000)
 
